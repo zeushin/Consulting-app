0b03376c41690c5fb41046f49e60c9d08b7e936e
Masher B <zeushin@gmail.com>
2012-01-31 15:04:54 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConsultingApp::Application.config.secret_token = '6e2171ed22a2ffc2c5afa8d543253a77abb2407d7143e0a39f73ade8e4ec3c590456f9e2d1868b828a535b6b31a9161b39746a90674c05bc6d081eaa5ae7ac0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ConsultingApp::Application.config.session_store :cookie_store, key: '_consulting_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ConsultingApp::Application.config.session_store :active_record_store
